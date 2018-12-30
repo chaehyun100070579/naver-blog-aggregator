7b3e8c767d5678d1c51f356c83d0b690fd4fcc9b
chaehyun <100070579@student.swin.edu.au>
2018-12-30 21:55:53 +0900

    validate duplicate username

---
+++ src/main/java/ch/software/nba/annotation/UniqueUsernameValidator.java
@@ -0,0 +1,27 @@
+package ch.software.nba.annotation;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import ch.software.nba.repository.UserRepository;
+
+public class UniqueUsernameValidator implements ConstraintValidator<UniqueUsername, String> {
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void initialize(UniqueUsername constraintAnnotation) {
+	}
+
+	@Override
+	public boolean isValid(String username, ConstraintValidatorContext context) {
+		if(userRepository == null) {
+			return true;
+		}
+		return userRepository.findByName(username) == null;
+	}
+
+}
